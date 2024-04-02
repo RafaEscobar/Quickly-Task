62dfe7a22854f2466e9229b1fe58ec40c7f3a6bc
Rafael Escobar Gutierrez <rafael.eg.code@gmail.com>
2024-04-01 20:16:31 -0600

    Avanzando con login

--- App.tsx
+++ App.tsx
@@ -1,118 +1,95 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-import type {PropsWithChildren} from 'react';
 import {
+  Image,
   SafeAreaView,
-  ScrollView,
-  StatusBar,
   StyleSheet,
   Text,
-  useColorScheme,
+  TextInput,
   View,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
-
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+export default function App(){
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-}
-
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
+    <SafeAreaView style={styles.contenedor}>
+      <View style={styles.subContainerA}>
+        <View style={styles.imgContainer}>
+          <Image source={require('./assets/imgs/logo-blue.png')} style={{width: 120, height:120}} />
         </View>
-      </ScrollView>
+        <View style={{display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: 12}}>
+          <Text style={{fontSize: 44}}>Registro</Text>
+        </View>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{
+              borderBottomWidth: 1,
+              borderBottomColor: 'gray'
+            }}
+            placeholder={'Nombre'}
+          />
+        </View>
+      </View>
+      <View style={styles.subContainerB}>
+        <Text>Holis</Text>
+      </View>
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  contenedor: {
+    backgroundColor: '#0CC0DF',
+    flex: 1,
+    flexDirection: 'column',
+    width: '100%',
+    height: '100%',
+    paddingTop: 40,
+    paddingBottom: 40,
+    paddingLeft: 20,
+    paddingRight: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    gap: 26,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  subContainerA: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '83.333%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    borderRadius: 20,
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  subContainerB: {
+    backgroundColor: 'white',
+    width: '100%',
+    height: '16.666%',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    borderRadius: 20,
   },
-  highlight: {
-    fontWeight: '700',
+  imgContainer: {
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'center',
+    marginTop: 26,
   },
-});
-
-export default App;
+  inputContainer: {
+    flexDirection: 'column',
+    justifyContent: 'center',
+    paddingTop: 34,
+    paddingLeft: 36,
+    paddingRight: 36,
+  }
+})
